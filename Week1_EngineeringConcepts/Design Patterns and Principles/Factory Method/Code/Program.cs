730a1ef73b9b02d9919f05fdc5c8a24e491e6007
Har-ry123 <hariomjha100@gmail.com>
2025-06-20 18:01:42 +0530

    Add files via upload

---
+++ Week1_EngineeringConcepts/Design Patterns and Principles/Factory Method/Code/Program.cs
@@ -0,0 +1,24 @@
+using System;
+namespace FactoryMethodPatternExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DocumentFactory factory = new DocumentFactory();
+
+            Document wordDocument = factory.CreateDocument("word");
+            wordDocument.PerformOperation();
+
+            Console.WriteLine();
+
+            Document pdfDocument = factory.CreateDocument("pdf");
+            pdfDocument.PerformOperation();
+
+            Console.WriteLine();
+
+            Document excelDocument = factory.CreateDocument("excel");
+            excelDocument.PerformOperation();
+        }
+    }
+}
