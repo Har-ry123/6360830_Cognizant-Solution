073f220b48a65c09ba4e963d3e844d58a7a945ae
Har-ry123 <hariomjha100@gmail.com>
2025-07-06 12:27:46 +0530

    Delete Week3_EntityFrameworkCore_8.0 directory

--- Week3_EntityFrameworkCore_8.0/Code/RetailInventory/Models/Category.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-public class Category {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public List<Product> Products { get; set; }
-}
