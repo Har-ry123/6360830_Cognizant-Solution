d685d54e10a14b4c2a6ed2a3a15efb9d1ab19f45
Har-ry123 <hariomjha100@gmail.com>
2025-07-11 17:42:15 +0530

    Add files via upload

---
+++ Week4_WebApi_Handson/2_WebApi_Handson/Code/WebApi2/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerDemoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]  // will change this to 'Emp' later
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly List<string> Employees = new()
+        {
+            "Alice", "Bob", "Charlie"
+        };
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult GetEmployees()
+        {
+            return Ok(Employees);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        public IActionResult AddEmployee([FromBody] string name)
+        {
+            Employees.Add(name);
+            return Ok(Employees);
+        }
+
+        // Demonstrate ActionName
+        [HttpGet("details")]
+        [ActionName("Details")]
+        public IActionResult GetDetails()
+        {
+            return Ok("This is the employee details endpoint.");
+        }
+    }
+}
+
