4821ed37b73362c4ed14731e73abae9261b6e26f
Har-ry123 <hariomjha100@gmail.com>
2025-07-27 17:47:27 +0530

    Add files via upload

---
+++ Week6_ReactJS-HOL/2.ReactJS/code/studentapp/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        minHeight: '100vh',
+        textAlign: 'center'
+      }}>
+        <Home />
+        <About />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
